570d3a23a77180ff1c8e61531c3d75c63fbca955
Parham <prmbas@gmail.com>
2026-05-04 00:02:32 +0000

    sample table for data

---
+++ infra/clickhouse/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE cicd_test_event_db;
+USE cicd_test_event_db;
+CREATE TABLE IF NOT EXISTS banner_view (
+    event_time DateTime,
+    banner_id UInt16,
+    user_id UInt64
+)
+ENGINE = MergeTree()
+ORDER BY event_time;
