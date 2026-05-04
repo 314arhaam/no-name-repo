f5f7947214ec1ffdadf98c229e69a7869d2eb988
Parham <prmbas@gmail.com>
2026-05-04 13:41:03 +0000

    ch connect version

--- services/reporter/reporter.py
+++ services/reporter/reporter.py
@@ -1,16 +1,16 @@
-from clickhouse_driver import Client
+from clickhouse_connect
 import dotenv, os
 
 if __name__ == '__main__':
     # load env file
     dotenv.load_dotenv()
     # create clickhouse client
-    client = Client(
+    client = clickhouse_connect.get_client(
         host="localhost",
-        port=9000,
-        user=os.getenv("CLICKHOUSE_USER"),
+        port=8123,
+        username=os.getenv("CLICKHOUSE_USER"),
         password=os.getenv("CLICKHOUSE_PASSWORD")
     )
     # fetch version
-    result = client.execute("SELECT * FROM cicd_test_event_db.banner_view limit 10")
+    result = client.query_df("SELECT * FROM cicd_test_event_db.banner_view limit 10")
     print(result)
