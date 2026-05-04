11eeb0060f7a0179fdc9a3c0562b3bf230592d94
Parham <prmbas@gmail.com>
2026-05-04 23:43:46 +0000

    sql write read done

--- services/static-ecommerce/main.py
+++ services/static-ecommerce/main.py
@@ -1,7 +1,8 @@
 import src.users
 import src.products
 import src.orders
-import sys, argparse
+import sys, argparse, os
+import sqlalchemy
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -49,9 +50,40 @@ if __name__ == "__main__":
     ).drop(
         columns = ["index", "user_id_"]
     )
-    if not args.sql:
+    if args.sql:
         print(users_df)
         print(products_df)
         print(orders_df)
     else:
-        print("Not implemented")
+        host = os.getenv("MYSQL_HOST", "localhost")
+        port = os.getenv("MYSQL_PORT", "3306")
+        database = os.getenv("MYSQL_DATABASE")
+        user = os.getenv("MYSQL_USER")
+        password = os.getenv("MYSQL_PASSWORD")
+        # SQLAlchemy engine string for MySQL (PyMySQL driver)
+        engine = create_engine(
+            f"mysql+pymysql://{user}:{password}@{host}:{port}",
+            echo=False,  # set to True to debug SQL
+            pool_recycle=3600
+        )
+        db = 'cicd_test_event_db'
+        table_name = "banner_view"
+        config = [
+            ("dim", "user", users_df),
+            ("dim", "product", products_df),
+            ("fact", "order", orders_df)
+        ]
+        # ---- Write DataFrame to MySQL ----
+        for conf in config:
+            with engine.begin() as conn:
+                conf[2].to_sql(
+                    name=conf[1],
+                    schema=conf[0],
+                    con=conn,
+                    if_exists="replace",
+                    index=False,
+                    method="multi"
+                )
+        # ---- Read data back into a DataFrame with proper column names ----
+        df_read = pd.read_sql(f"SELECT * FROM fact.order", con=engine)
+        print(df_read)
