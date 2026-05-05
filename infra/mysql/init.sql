885d688a878a6b62f0c52827d8e00751383486da
Parham <prmbas@gmail.com>
2026-05-05 01:18:55 +0000

    col name fixed

--- infra/mysql/init.sql
+++ infra/mysql/init.sql
@@ -29,7 +29,7 @@ CREATE DATABASE IF NOT EXISTS fact;
 CREATE TABLE IF NOT EXISTS fact.order (
     id_ INT PRIMARY KEY,
     order_id INT,
-    created_at DATETIME,
+    create_at DATETIME,
     order_status VARCHAR(50),
     user_id INT,
     product_id INT
