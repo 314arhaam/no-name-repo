ee655dc40d30fc9c7754404ccbdfa43fecf943ea
Parham <prmbas@gmail.com>
2026-05-05 20:22:35 +0000

    drivers added

---
+++ services/reporter/app/src/clickhouse.py
@@ -0,0 +1,24 @@
+import clickhouse_connect
+import os
+import pandas as pd
+
+class ClickHouse(DB):
+    def __init__(self):
+        super().__init__()
+        client = clickhouse_connect.get_client(
+            host=os.getenv("CLICKHOUSE_HOST", "localhost"),
+            port=os.getenv("CLICKHOUSE_PORT", "8123"),
+            username=os.getenv("CLICKHOUSE_USER"),
+            password=os.getenv("CLICKHOUSE_PASSWORD")
+        )
+        self.client = client
+    
+    def query_df(self, query: str = "SELECT 1 as test_col") -> pd.DataFrame:
+        return self.client.query_df(query)
+    
+    def ping(self):
+        return all(self.query_df() == pd.DataFrame({"test_col": 1}))
+
+if __name__ == '__main__':
+    ch = ClickHouse()
+    print(ch.ping())
