063478bfb2c7a01de1118e66b37ec53038f626e9
Parham <prmbas@gmail.com>
2026-05-06 22:32:17 +0000

    spark submitter

---
+++ services/pyspark-etl/app/main.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = SparkSession.builder \
+    .appName("MySQL_to_ClickHouse_ETL") \
+    .config("spark.jars", "mysql-connector-j-8.3.0.jar,clickhouse-jdbc-0.6.3.jar") \
+    .getOrCreate()
+
+source_db = "fact"
+source_table = "order"
+
+mysql_df = spark.read \ 
+    .format("jdbc") \
+    .option("url", f"jdbc:mysql://{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{source_db}") \
+    .option("driver", "com.mysql.cj.jdbc.Driver") \
+    .option("dbtable", source_table) \
+    .option("user", f"{os.getenv('MYSQL_USER')}") \
+    .option("password", f"{os.getenv('MYSQL_PASSWORD')}") \
+    .load()
+
+print("[*] Loaded from MySQL:")
+mysql_df.printSchema()
+mysql_df.show(5)
+
+transformed_df = mysql_df \
+    .groupBy("create_at") \
+    .agg(F.countDistinct("order_id").alias("order_count")) \
+    .withColumnRenamed("create_at", "date_")
+
+sink_db = "analytics"
+sink_table = "daily_order"
+
+transformed_df.write \
+    .format("jdbc") \
+    .mode("append") \
+    .option("url", f"jdbc:ch://{os.getenv('CLICKHOUSE_HOST')}:{os.getenv('CLICKHOUSE_PORT')}/{sink_db}") \
+    .option("driver", "com.clickhouse.jdbc.ClickHouseDriver") \
+    .option("dbtable", sink_table) \
+    .option("user", f"{os.getenv('CLICKHOUSE_USER')}") \
+    .option("password", f"{os.getenv('CLICKHOUSE_PASSWORD')}") \
+    .option("batchsize", 100000) \
+    .save()
+
+print("[*] Data written to ClickHouse successfully")
+
+spark.stop()
