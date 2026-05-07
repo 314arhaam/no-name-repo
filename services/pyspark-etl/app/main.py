3b55e7a98d886ed798a6b63b48148aed07d72099
Parham <prmbas@gmail.com>
2026-05-07 17:46:12 +0000

    fix

--- services/pyspark-etl/app/main.py
+++ services/pyspark-etl/app/main.py
@@ -38,10 +38,9 @@ sink_table = "daily_order"
 transformed_df.write \
     .format("jdbc") \
     .mode("append") \
-    .option("url", f"jdbc:clickhouse:http://clickhouse_server:{os.getenv('CLICKHOUSE_PORT')}/{sink_db}") \
+    .option("url", f"jdbc:ch://clickhouse_server:{os.getenv('CLICKHOUSE_PORT')}/{sink_db}") \
     .option("driver", "com.clickhouse.jdbc.ClickHouseDriver") \
     .option("dbtable", f"{sink_table}") \
-    .option("http_connection_provider", "HTTP_URL_CONNECTION") \
     .option("user", f"{os.getenv('CLICKHOUSE_USER')}") \
     .option("password", f"{os.getenv('CLICKHOUSE_PASSWORD')}") \
     .option("batchsize", 100000) \
