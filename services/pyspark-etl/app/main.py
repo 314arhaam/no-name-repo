f3698a85216090a3b556d3ac180e8ebf94f5811a
Parham <prmbas@gmail.com>
2026-05-07 03:19:40 +0000

    ch server meta false

--- services/pyspark-etl/app/main.py
+++ services/pyspark-etl/app/main.py
@@ -38,7 +38,7 @@ sink_table = "daily_order"
 transformed_df.write \
     .format("jdbc") \
     .mode("append") \
-    .option("url", f"jdbc:clickhouse://clickhouse_server:{os.getenv('CLICKHOUSE_PORT')}/{sink_db}") \
+    .option("url", f"jdbc:clickhouse://clickhouse_server:{os.getenv('CLICKHOUSE_PORT')}/{sink_db}?use_server_metadata=false") \
     .option("driver", "com.clickhouse.jdbc.ClickHouseDriver") \
     .option("dbtable", f"{sink_db}.{sink_table}") \
     .option("user", f"{os.getenv('CLICKHOUSE_USER')}") \
