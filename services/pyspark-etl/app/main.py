3e08e3abf8121fb37a65137d93cefeaff35dcf52
Parham <prmbas@gmail.com>
2026-05-07 03:39:56 +0000

    ch driver test

--- services/pyspark-etl/app/main.py
+++ services/pyspark-etl/app/main.py
@@ -38,7 +38,7 @@ sink_table = "daily_order"
 transformed_df.write \
     .format("jdbc") \
     .mode("append") \
-    .option("url", f"http://clickhouse_server:{os.getenv('CLICKHOUSE_PORT')}/{sink_db}") \
+    .option("url", f"jdbc:clickhouse://clickhouse_server:{os.getenv('CLICKHOUSE_PORT')}/{sink_db}") \
     .option("driver", "com.clickhouse.jdbc.ClickHouseDriver") \
     .option("dbtable", f"{sink_db}.{sink_table}") \
     .option("user", f"{os.getenv('CLICKHOUSE_USER')}") \
