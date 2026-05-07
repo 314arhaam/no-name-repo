ce1dfba2093de982bd9aa42bfeabd11bcd3ffa07
Parham <prmbas@gmail.com>
2026-05-07 01:42:55 +0000

    test for spark

--- services/pyspark-etl/app/test.py
+++ services/pyspark-etl/app/test.py
@@ -1,1 +1,10 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = SparkSession.builder \
+    .appName("Test_Job") \
+    .getOrCreate()
+
 print("HELLO")
+
+spark.stop()
